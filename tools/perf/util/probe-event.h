30432c828f74001b217e1767245f5d7a5847c87c
Jamie Iles <jamie.iles@oracle.com>
2017-11-08 12:41:56 +0000

    RHEL: perf fixes.
    - [tools] perf ppc64le: Fix build failure when libelf is not present (Jiri Olsa) [1414710 1376534]
    - [tools] perf probe ppc64le: Fix probe location when using DWARF (Jiri Olsa) [1414710 1376534]
    - [tools] perf probe: Add function to post process kernel trace events (Jiri Olsa) [1414710 1376534]
    - [tools] perf symbols: Fix kallsyms perf test on ppc64le (Jiri Olsa) [1414710 1376534]
    - [tools] perf powerpc: Fix kprobe and kretprobe handling with kallsyms on ppc64le (Jiri Olsa) [1414710 1376534]
    
    Change-Id: I4e0387995111a89c76d7e812461d94514e88cb4b
    Signed-off-by: Jamie Iles <jamie.iles@oracle.com>

--- tools/perf/util/probe-event.h
+++ tools/perf/util/probe-event.h
@@ -152,9 +152,9 @@ int show_line_range(struct line_range *lr, const char *module, bool user);
 int show_available_vars(struct perf_probe_event *pevs, int npevs,
 			struct strfilter *filter);
 int show_available_funcs(const char *module, struct strfilter *filter, bool user);
-bool arch__prefers_symtab(void);
 void arch__fix_tev_from_maps(struct perf_probe_event *pev,
-			     struct probe_trace_event *tev, struct map *map);
+			     struct probe_trace_event *tev, struct map *map,
+			     struct symbol *sym);
 
 /* If there is no space to write, returns -E2BIG. */
 int e_snprintf(char *str, size_t size, const char *format, ...)
@@ -165,5 +165,10 @@ int e_snprintf(char *str, size_t size, const char *format, ...)
 
 int copy_to_probe_trace_arg(struct probe_trace_arg *tvar,
 			    struct perf_probe_arg *pvar);
+
+struct map *get_target_map(const char *target, bool user);
+
+void arch__post_process_probe_trace_events(struct perf_probe_event *pev,
+					   int ntevs);
 
 #endif /*_PROBE_EVENT_H */
