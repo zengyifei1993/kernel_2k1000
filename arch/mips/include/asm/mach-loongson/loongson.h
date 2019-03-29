676b13e8d388b98e766d1c1533ea99b579e79cf9
Juxin Gao <gaojuxin@loongson.cn>
2019-03-29 18:08:57 +0800

    Loongson-3: 3A4000: Add temperature csr access support.
    Change-Id: Ib36d518774b89b73b29c2e7c5abceac5a40f9fd0
    Signed-off-by: Juxin Gao <gaojuxin@loongson.cn>

--- arch/mips/include/asm/mach-loongson/loongson.h
+++ arch/mips/include/asm/mach-loongson/loongson.h
@@ -167,6 +167,7 @@ static inline void do_perfcnt_IRQ(void)
 
 /* Register offset and bit definition for CSR access */
 #define LOONGSON_CPU_FEATURE_OFFSET		0x00000008
+#define LOONGSON_CPU_TEMPERATURE_OFFSET		0x00000428
 #define LOONGSON_CPU_FEATURE_IPI_PERCORE	(1ULL << 4)
 
 /* Loongson h/w Configuration */
