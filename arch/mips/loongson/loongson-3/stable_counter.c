a0bd133f8e13373447973d62851c6fd53e0da236
yangyinglu <yangyinglu@loongson.cn>
2019-04-12 17:57:42 +0800

    Loongson-3: 3A4000: Add Stable Timer csr access support
    Stabel Timer is a new clock source in 3A4000. It is
    constant frequency timer that replaces r4k_clockevent.
    Stable Timer and Stable counter are implemented
    simultaneously and the frequency is consistent.
    
    Change-Id: I351f2bda303846b64fd128edc4be5430b40e159e
    Signed-off-by: yangyinglu <yangyinglu@loongson.cn>

--- arch/mips/loongson/loongson-3/stable_counter.c
+++ arch/mips/loongson/loongson-3/stable_counter.c
@@ -2,9 +2,162 @@
 #include <asm/time.h>
 #include <loongson.h>
 #include <asm/mipsregs.h>
+#include <asm/cevt-r4k.h>
 
 #define ls_stable_freq cpu_clock_freq
 
+#define STABLE_TIMER_PERIODIC_EN    (_ULCAST_(1) << 62)
+#define STABLE_TIMER_EN             (_ULCAST_(1) << 61)
+#define GSCFG_FLT_EN                (_ULCAST_(1) << 7)
+#define STABLE_TIMER_CFG            0x1060
+#define STABLE_PERIODIC_INIT        0x100000
+
+#define stabel_irq    7
+
+int stable_timer_irq_installed;
+
+static DEFINE_SPINLOCK(stable_lock);
+DEFINE_PER_CPU(struct clock_event_device, stable_clockevent_device);
+
+irqreturn_t stable_irq_handler(int irq, void *data)
+{
+    const int r2 = cpu_has_mips_r2;
+    struct clock_event_device *cd;
+    int cpu = smp_processor_id();
+
+	/*
+	 * Suckage alert:
+	 * Before R2 of the architecture there was no way to see if a
+	 * performance counter interrupt was pending, so we have to run
+	 * the performance counter interrupt handler anyway.
+	 */
+	if (handle_perf_irq(r2))
+		goto out;
+
+	/*
+	 * The same applies to performance counter interrupts.	But with the
+	 * above we now know that the reason we got here must be a timer
+	 * interrupt.  Being the paranoiacs we are we check anyway.
+	 */
+    if (!r2 || (read_c0_cause() & (1 << 30))) {
+        /* Clear Count/Compare Interrupt */
+        write_c0_compare(read_c0_compare());
+        cd = &per_cpu(stable_clockevent_device, cpu);
+        cd->event_handler(cd);
+    }
+
+out:
+    return IRQ_HANDLED;
+}
+
+static struct irqaction stable_irq_irqaction = {
+    .handler = stable_irq_handler,
+    .flags = IRQF_PERCPU | IRQF_SHARED | IRQF_TIMER,
+    .name = "stable timer",
+};
+
+void stable_event_handler(struct clock_event_device *dev)
+{
+}
+
+static void stable_set_mode(enum clock_event_mode mode,
+                struct clock_event_device *evt)
+{
+    unsigned long  cfg;
+
+    spin_lock(&stable_lock);
+
+    switch (mode) {
+        case CLOCK_EVT_MODE_PERIODIC:
+            printk(KERN_INFO "set stable clock event to periodic mode!\n");
+            cfg = dread_csr(STABLE_TIMER_CFG);
+            cfg |= (STABLE_TIMER_PERIODIC_EN | STABLE_TIMER_EN);
+            dwrite_csr(STABLE_TIMER_CFG, cfg | STABLE_PERIODIC_INIT);
+            break;
+        case CLOCK_EVT_MODE_ONESHOT:
+            printk(KERN_INFO "set stable clock event to oneshot mode!\n");
+            cfg = dread_csr(STABLE_TIMER_CFG);
+            /* set timer0 type
+             * 1 : periodic interrupt
+             * 0 : non-periodic(oneshot) interrupt
+             */
+            cfg &= ~STABLE_TIMER_PERIODIC_EN;
+            cfg |= STABLE_TIMER_EN;
+            dwrite_csr(STABLE_TIMER_CFG, cfg);
+            break;
+        case CLOCK_EVT_MODE_SHUTDOWN:
+        case CLOCK_EVT_MODE_UNUSED:
+        case CLOCK_EVT_MODE_RESUME:
+            break;
+    }
+    spin_unlock(&stable_lock);
+}
+
+static int stable_next_event(unsigned long delta,
+                struct clock_event_device *evt)
+{
+    dread_csr(STABLE_TIMER_CFG);
+    dwrite_csr(STABLE_TIMER_CFG, delta | STABLE_TIMER_EN);
+
+    return 0;
+}
+
+int stable_clockevent_init(void)
+{
+	struct cpuinfo_mips *c = &current_cpu_data;
+	unsigned int cpu = smp_processor_id();
+	struct clock_event_device *cd;
+    unsigned int irq;
+
+    if (!ls_stable_freq)
+        return -ENXIO;
+
+	switch (c->cputype) {
+	case CPU_LOONGSON3:
+		/* Loongson3A2000 and Loongson3A3000 has no stable timer */
+        return -ENXIO;
+	case CPU_LOONGSON3_COMP:
+		break;
+	default:
+        return -ENXIO;
+	}
+
+    if (!(read_cfg(LOONGSON_CPUCFG_CONFIG_FIELD2) & MIPS_LSE_LLFTP)) {
+        printk(KERN_INFO "Don't support STABLE timer\n");
+        return -EPERM;
+    }
+
+    if (!(read_c0_config6() & GSCFG_FLT_EN)) {
+        printk(KERN_INFO "GSCFG_FLTInt not set\n");
+        return -EPERM;
+    }
+
+    irq = MIPS_CPU_IRQ_BASE + stabel_irq;
+
+	cd = &per_cpu(stable_clockevent_device, cpu);
+
+	cd->name = "stable timer";
+	cd->features = CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT;
+
+	cd->rating = 320;
+    cd->irq = irq;
+    cd->cpumask = cpumask_of(cpu);
+	cd->set_mode = stable_set_mode;
+	cd->set_next_event = stable_next_event;
+    cd->event_handler   = stable_event_handler;
+
+    clockevents_config_and_register(cd, ls_stable_freq, 0x300, ((1UL << 48) - 1));
+
+	if (stable_timer_irq_installed)
+		return 0;
+
+	stable_timer_irq_installed = 1;
+    setup_irq(irq, &stable_irq_irqaction);
+
+    printk(KERN_INFO "stable clock event device register\n");
+
+    return 0;
+}
 
 static void stable_counter_suspend(struct clocksource *cs)
 {
@@ -20,7 +173,7 @@ static cycle_t stable_counter_csr_read(struct clocksource *cs)
 }
 
 static struct clocksource csrc_stable_counter = {
-	.name = "stable",
+	.name = "stable counter",
 	/* mips clocksource rating is less than 300, so stable_counter is better. */
 	.rating = 380,
 	.read = stable_counter_csr_read,
