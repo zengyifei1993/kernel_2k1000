fdac8bca4ba9a48871b07027a23a084c29da66dc
Lixing <lixing@loongson.cn>
2019-09-05 10:53:59 +0800

    Loongson-3: Divide tlb operations for 3A3000 GUEST from host
    Change all tlb operations for 3A3000 guest to seperate interface
    other than the host, more clean code style.
    
    Change-Id: Iff14d67fd33192b422da9ddec41ec87a4a8aef85
    Signed-off-by: Lixing <lixing@loongson.cn>

--- arch/mips/mm/tlb-r4k.c
+++ arch/mips/mm/tlb-r4k.c
@@ -25,32 +25,6 @@
 
 extern void build_tlb_refill_handler(void);
 
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-static noinline void emulate_tlb_ops(unsigned long address,
-			    unsigned long pageshift, unsigned long even_pte,
-			    unsigned long odd_pte, int op_type,
-			    unsigned long flags)
-{
-	__asm__ __volatile__(
-	"	.set	push			\n"
-	"	.set	noreorder		\n"
-	"	move	$2, %[A4]		\n"
-	"	move	$3, %[A5]		\n"
-	"	move	$4, %[A0]		\n"
-	"	move	$5, %[A1]		\n"
-	"	move	$6, %[A2]		\n"
-	"	move	$7, %[A3]		\n"
-	"	.word	0x42000028		\n" //hypcall
-	"	nop				\n"
-	"	.set	reorder			\n"
-	"	.set	pop			\n"
-	:
-	: [A0] "r" (address), [A1] "r" (pageshift), [A2] "r" (even_pte),
-	  [A3] "r" (odd_pte), [A4] "r" (op_type), [A5] "r" (flags)
-	: "$2", "$3", "$4", "$5", "$6", "$7", "$8", "$9");
-}
-#endif
-
 /* Atomicity and interruptability */
 #ifdef CONFIG_MIPS_MT_SMTC
 
@@ -94,23 +68,203 @@ static inline void local_flush_tlb_all_fast(void)
 	change_c0_diag(0x300c, 0x300c);
 }
 
+#ifdef CONFIG_KVM_GUEST_LS3A3000
+static noinline void emulate_tlb_ops(unsigned long address,
+			    unsigned long pageshift, unsigned long even_pte,
+			    unsigned long odd_pte, int op_type,
+			    unsigned long flags)
+{
+	__asm__ __volatile__(
+	"	.set	push			\n"
+	"	.set	noreorder		\n"
+	"	move	$2, %[A4]		\n"
+	"	move	$3, %[A5]		\n"
+	"	move	$4, %[A0]		\n"
+	"	move	$5, %[A1]		\n"
+	"	move	$6, %[A2]		\n"
+	"	move	$7, %[A3]		\n"
+	"	.word	0x42000028		\n" //hypcall
+	"	nop				\n"
+	"	.set	reorder			\n"
+	"	.set	pop			\n"
+	:
+	: [A0] "r" (address), [A1] "r" (pageshift), [A2] "r" (even_pte),
+	  [A3] "r" (odd_pte), [A4] "r" (op_type), [A5] "r" (flags)
+	: "$2", "$3", "$4", "$5", "$6", "$7", "$8", "$9");
+}
 
 void local_flush_tlb_all(void)
 {
 	unsigned long flags;
-#ifndef CONFIG_KVM_GUEST_LS3A3000
+
+	ENTER_CRITICAL(flags);
+
+	emulate_tlb_ops(0, 1088, 0, 0, 0x5002, 2);
+	EXIT_CRITICAL(flags);
+}
+EXPORT_SYMBOL(local_flush_tlb_all);
+
+void local_flush_tlb_range(struct vm_area_struct *vma, unsigned long start,
+	unsigned long end)
+{
+	struct mm_struct *mm = vma->vm_mm;
+	int cpu = smp_processor_id();
+
+	if (cpu_context(cpu, mm) != 0) {
+		unsigned long size, flags;
+
+		ENTER_CRITICAL(flags);
+		start = round_down(start, PAGE_SIZE << 1);
+		end = round_up(end, PAGE_SIZE << 1);
+		size = (end - start) >> (PAGE_SHIFT + 1);
+		if (size <= (current_cpu_data.tlbsizeftlbsets ?
+			     current_cpu_data.tlbsize / 8 :
+			     current_cpu_data.tlbsize / 2)) {
+			int oldpid = read_c0_entryhi();
+			int newpid = cpu_asid(cpu, mm);
+
+			htw_stop();
+			write_c0_entryhi(newpid);
+			emulate_tlb_ops(start, end, size, 0, 0x5003, 2);
+			tlbw_use_hazard();
+			write_c0_entryhi(oldpid);
+			htw_start();
+		} else {
+			drop_mmu_context(mm, cpu);
+		}
+		EXIT_CRITICAL(flags);
+	}
+}
+
+void local_flush_tlb_kernel_range(unsigned long start, unsigned long end)
+{
+	unsigned long size, flags;
+
+	ENTER_CRITICAL(flags);
+	size = (end - start + (PAGE_SIZE - 1)) >> PAGE_SHIFT;
+	size = (size + 1) >> 1;
+	if (size <= (current_cpu_data.tlbsizeftlbsets ?
+		     current_cpu_data.tlbsize / 8 :
+		     current_cpu_data.tlbsize / 2)) {
+
+		start &= (PAGE_MASK << 1);
+		end += ((PAGE_SIZE << 1) - 1);
+		end &= (PAGE_MASK << 1);
+		htw_stop();
+
+		emulate_tlb_ops(start, end, size, 0, 0x5004, 2);
+		htw_start();
+	} else {
+		local_flush_tlb_all();
+	}
+	EXIT_CRITICAL(flags);
+}
+
+void local_flush_tlb_page(struct vm_area_struct *vma, unsigned long page)
+{
+	int cpu = smp_processor_id();
+
+	if (cpu_context(cpu, vma->vm_mm) != 0) {
+		unsigned long flags;
+		int oldpid, newpid;
+
+		newpid = cpu_asid(cpu, vma->vm_mm);
+		ENTER_CRITICAL(flags);
+		oldpid = read_c0_entryhi();
+		htw_stop();
+		write_c0_entryhi(newpid);
+
+		emulate_tlb_ops(page, page, 0, 0, 0x5001, 2);
+		write_c0_entryhi(oldpid);
+		htw_start();
+		flush_micro_tlb_vm(vma);
+		EXIT_CRITICAL(flags);
+	}
+}
+
+/*
+ * This one is only used for pages with the global bit set so we don't care
+ * much about the ASID.
+ */
+void local_flush_tlb_one(unsigned long page)
+{
+	unsigned long flags;
+	int oldpid;
+
+	ENTER_CRITICAL(flags);
+	oldpid = read_c0_entryhi();
+	htw_stop();
+	emulate_tlb_ops(page, page, 0, 0, 0x5005, 2);
+	write_c0_entryhi(oldpid);
+	htw_start();
+	EXIT_CRITICAL(flags);
+}
+
+/*
+ * We will need multiple versions of update_mmu_cache(), one that just
+ * updates the TLB with the new pte(s), and another which also checks
+ * for the R4k "end of page" hardware bug and does the needy.
+ */
+void __update_tlb(struct vm_area_struct * vma, unsigned long address, pte_t pte)
+{
+	unsigned long flags;
+	pgd_t *pgdp;
+	pud_t *pudp;
+	pmd_t *pmdp;
+	pte_t *ptep;
+	unsigned long pageshift,even_pte,odd_pte;
+	unsigned long tmp_address;
+	unsigned long vm_flags;
+
+	/*
+	 * Handle debugger faulting in for debugee.
+	 */
+	if (current->active_mm != vma->vm_mm)
+		return;
+
+	ENTER_CRITICAL(flags);
+
+	htw_stop();
+
+	tmp_address = address;
+	address &= (PAGE_MASK << 1);
+	pgdp = pgd_offset(vma->vm_mm, address);
+	pudp = pud_offset(pgdp, address);
+	pmdp = pmd_offset(pudp, address);
+
+	ptep = pte_offset_map(pmdp, address);
+	pageshift= 14;
+	even_pte = pte_val(*ptep++);
+	odd_pte = pte_val(*ptep);
+
+	if((tmp_address >> PAGE_SHIFT) & 0x1)
+		if(odd_pte & _PAGE_DIRTY)
+			vm_flags = 3;
+		else
+			vm_flags = 2;
+	else
+		if(even_pte & _PAGE_DIRTY)
+			vm_flags = 3;
+		else
+			vm_flags = 2;
+
+	emulate_tlb_ops(tmp_address, pageshift, even_pte, odd_pte, 0x4000, vm_flags);
+	htw_start();
+	flush_micro_tlb_vm(vma);
+	EXIT_CRITICAL(flags);
+}
+#else
+void local_flush_tlb_all(void)
+{
+	unsigned long flags;
 	int entry;
 	unsigned long old_ctx;
 	int ftlbhighset;
-#endif
 
 
 	ENTER_CRITICAL(flags);
 
 	/* Blast 'em all away. */
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-	emulate_tlb_ops(0, 1088, 0, 0, 0x5002, 2);
-#else
 	/* Save old context and create impossible VPN2 value */
 	htw_stop();
 	entry = read_c0_wired();
@@ -193,10 +347,250 @@ void local_flush_tlb_all(void)
 	}
 	flush_micro_tlb();
 	htw_start();
-#endif
 	EXIT_CRITICAL(flags);
 }
 EXPORT_SYMBOL(local_flush_tlb_all);
+
+void local_flush_tlb_range(struct vm_area_struct *vma, unsigned long start,
+	unsigned long end)
+{
+	struct mm_struct *mm = vma->vm_mm;
+	int cpu = smp_processor_id();
+
+	if (cpu_context(cpu, mm) != 0) {
+		unsigned long size, flags;
+
+		ENTER_CRITICAL(flags);
+		start = round_down(start, PAGE_SIZE << 1);
+		end = round_up(end, PAGE_SIZE << 1);
+		size = (end - start) >> (PAGE_SHIFT + 1);
+		if (size <= (current_cpu_data.tlbsizeftlbsets ?
+			     current_cpu_data.tlbsize / 8 :
+			     current_cpu_data.tlbsize / 2)) {
+			int oldpid = read_c0_entryhi();
+			int newpid = cpu_asid(cpu, mm);
+
+			htw_stop();
+
+			while (start < end) {
+				int idx;
+
+				write_c0_entryhi(start | newpid);
+				start += (PAGE_SIZE << 1);
+				mtc0_tlbw_hazard();
+				tlb_probe();
+				tlb_probe_hazard();
+				idx = read_c0_index();
+				write_c0_entrylo0(0);
+				write_c0_entrylo1(0);
+				if (idx < 0)
+					continue;
+				/* Make sure all entries differ. */
+				write_c0_entryhi(UNIQUE_ENTRYHI(idx));
+				mtc0_tlbw_hazard();
+				tlb_write_indexed();
+			}
+			tlbw_use_hazard();
+			write_c0_entryhi(oldpid);
+			htw_start();
+		} else {
+			drop_mmu_context(mm, cpu);
+		}
+		flush_micro_tlb();
+		EXIT_CRITICAL(flags);
+	}
+}
+
+void local_flush_tlb_kernel_range(unsigned long start, unsigned long end)
+{
+	unsigned long size, flags;
+
+	ENTER_CRITICAL(flags);
+	size = (end - start + (PAGE_SIZE - 1)) >> PAGE_SHIFT;
+	size = (size + 1) >> 1;
+	if (size <= (current_cpu_data.tlbsizeftlbsets ?
+		     current_cpu_data.tlbsize / 8 :
+		     current_cpu_data.tlbsize / 2)) {
+		int pid = read_c0_entryhi();
+
+		start &= (PAGE_MASK << 1);
+		end += ((PAGE_SIZE << 1) - 1);
+		end &= (PAGE_MASK << 1);
+		htw_stop();
+
+		while (start < end) {
+			int idx;
+
+			write_c0_entryhi(start);
+			start += (PAGE_SIZE << 1);
+			mtc0_tlbw_hazard();
+			tlb_probe();
+			tlb_probe_hazard();
+			idx = read_c0_index();
+			write_c0_entrylo0(0);
+			write_c0_entrylo1(0);
+			if (idx < 0)
+				continue;
+			/* Make sure all entries differ. */
+			write_c0_entryhi(UNIQUE_ENTRYHI(idx));
+			mtc0_tlbw_hazard();
+			tlb_write_indexed();
+		}
+		tlbw_use_hazard();
+		write_c0_entryhi(pid);
+		htw_start();
+	} else {
+		local_flush_tlb_all();
+	}
+	flush_micro_tlb();
+	EXIT_CRITICAL(flags);
+}
+
+void local_flush_tlb_page(struct vm_area_struct *vma, unsigned long page)
+{
+	int cpu = smp_processor_id();
+
+	if (cpu_context(cpu, vma->vm_mm) != 0) {
+		unsigned long flags;
+		int oldpid, newpid, idx;
+
+		newpid = cpu_asid(cpu, vma->vm_mm);
+		page &= (PAGE_MASK << 1);
+		ENTER_CRITICAL(flags);
+		oldpid = read_c0_entryhi();
+		htw_stop();
+		write_c0_entryhi(page | newpid);
+		mtc0_tlbw_hazard();
+		tlb_probe();
+		tlb_probe_hazard();
+		idx = read_c0_index();
+		write_c0_entrylo0(0);
+		write_c0_entrylo1(0);
+		if (idx < 0)
+			goto finish;
+		/* Make sure all entries differ. */
+		write_c0_entryhi(UNIQUE_ENTRYHI(idx));
+		mtc0_tlbw_hazard();
+		tlb_write_indexed();
+		tlbw_use_hazard();
+
+	finish:
+		write_c0_entryhi(oldpid);
+		htw_start();
+		flush_micro_tlb_vm(vma);
+		EXIT_CRITICAL(flags);
+	}
+}
+
+/*
+ * This one is only used for pages with the global bit set so we don't care
+ * much about the ASID.
+ */
+void local_flush_tlb_one(unsigned long page)
+{
+	unsigned long flags;
+	int oldpid, idx;
+
+	ENTER_CRITICAL(flags);
+	oldpid = read_c0_entryhi();
+	htw_stop();
+	page &= (PAGE_MASK << 1);
+	write_c0_entryhi(page);
+	mtc0_tlbw_hazard();
+	tlb_probe();
+	tlb_probe_hazard();
+	idx = read_c0_index();
+	write_c0_entrylo0(0);
+	write_c0_entrylo1(0);
+	if (idx >= 0) {
+		/* Make sure all entries differ. */
+		write_c0_entryhi(UNIQUE_ENTRYHI(idx));
+		mtc0_tlbw_hazard();
+		tlb_write_indexed();
+		tlbw_use_hazard();
+	}
+	write_c0_entryhi(oldpid);
+	htw_start();
+	flush_micro_tlb();
+	EXIT_CRITICAL(flags);
+}
+
+/*
+ * We will need multiple versions of update_mmu_cache(), one that just
+ * updates the TLB with the new pte(s), and another which also checks
+ * for the R4k "end of page" hardware bug and does the needy.
+ */
+void __update_tlb(struct vm_area_struct * vma, unsigned long address, pte_t pte)
+{
+	unsigned long flags;
+	pgd_t *pgdp;
+	pud_t *pudp;
+	pmd_t *pmdp;
+	pte_t *ptep;
+	int idx, pid;
+
+	/*
+	 * Handle debugger faulting in for debugee.
+	 */
+	if (current->active_mm != vma->vm_mm)
+		return;
+
+	ENTER_CRITICAL(flags);
+
+	htw_stop();
+
+	pid = read_c0_entryhi() & cpu_asid_mask(&current_cpu_data);
+	address &= (PAGE_MASK << 1);
+	write_c0_entryhi(address | pid);
+	pgdp = pgd_offset(vma->vm_mm, address);
+	mtc0_tlbw_hazard();
+	tlb_probe();
+	tlb_probe_hazard();
+	pudp = pud_offset(pgdp, address);
+	pmdp = pmd_offset(pudp, address);
+	idx = read_c0_index();
+#ifdef CONFIG_MIPS_HUGE_TLB_SUPPORT
+	/* this could be a huge page  */
+	if (pmd_huge(*pmdp)) {
+		unsigned long lo;
+		write_c0_pagemask(PM_HUGE_MASK);
+		ptep = (pte_t *)pmdp;
+		lo = pte_to_entrylo(pte_val(*ptep));
+		write_c0_entrylo0(lo);
+		write_c0_entrylo1(lo + (HPAGE_SIZE >> 7));
+
+		mtc0_tlbw_hazard();
+		if (idx < 0)
+			tlb_write_random();
+		else
+			tlb_write_indexed();
+		tlbw_use_hazard();
+		write_c0_pagemask(PM_DEFAULT_MASK);
+	} else
+#endif
+	{
+		ptep = pte_offset_map(pmdp, address);
+
+#if defined(CONFIG_64BIT_PHYS_ADDR) && defined(CONFIG_CPU_MIPS32)
+		write_c0_entrylo0(ptep->pte_high);
+		ptep++;
+		write_c0_entrylo1(ptep->pte_high);
+#else
+		write_c0_entrylo0(pte_to_entrylo(pte_val(*ptep++)));
+		write_c0_entrylo1(pte_to_entrylo(pte_val(*ptep)));
+#endif
+		mtc0_tlbw_hazard();
+		if (idx < 0)
+			tlb_write_random();
+		else
+			tlb_write_indexed();
+	}
+	tlbw_use_hazard();
+	htw_start();
+	flush_micro_tlb_vm(vma);
+	EXIT_CRITICAL(flags);
+}
+#endif
 
 static void local_flush_tlb_mm_slow(unsigned long asid)
 {
@@ -261,307 +655,6 @@ void local_flush_tlb_mm(struct mm_struct *mm)
 	}
 
 	preempt_enable();
-}
-
-void local_flush_tlb_range(struct vm_area_struct *vma, unsigned long start,
-	unsigned long end)
-{
-	struct mm_struct *mm = vma->vm_mm;
-	int cpu = smp_processor_id();
-
-	if (cpu_context(cpu, mm) != 0) {
-		unsigned long size, flags;
-
-		ENTER_CRITICAL(flags);
-		start = round_down(start, PAGE_SIZE << 1);
-		end = round_up(end, PAGE_SIZE << 1);
-		size = (end - start) >> (PAGE_SHIFT + 1);
-		if (size <= (current_cpu_data.tlbsizeftlbsets ?
-			     current_cpu_data.tlbsize / 8 :
-			     current_cpu_data.tlbsize / 2)) {
-			int oldpid = read_c0_entryhi();
-			int newpid = cpu_asid(cpu, mm);
-
-			htw_stop();
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-			write_c0_entryhi(newpid);
-			emulate_tlb_ops(start, end, size, 0, 0x5003, 2);
-#else
-			while (start < end) {
-				int idx;
-
-				write_c0_entryhi(start | newpid);
-				start += (PAGE_SIZE << 1);
-				mtc0_tlbw_hazard();
-				tlb_probe();
-				tlb_probe_hazard();
-				idx = read_c0_index();
-				write_c0_entrylo0(0);
-				write_c0_entrylo1(0);
-				if (idx < 0)
-					continue;
-				/* Make sure all entries differ. */
-				write_c0_entryhi(UNIQUE_ENTRYHI(idx));
-				mtc0_tlbw_hazard();
-				tlb_write_indexed();
-			}
-#endif
-			tlbw_use_hazard();
-			write_c0_entryhi(oldpid);
-			htw_start();
-		} else {
-			drop_mmu_context(mm, cpu);
-		}
-		flush_micro_tlb();
-		EXIT_CRITICAL(flags);
-	}
-}
-
-void local_flush_tlb_kernel_range(unsigned long start, unsigned long end)
-{
-	unsigned long size, flags;
-
-	ENTER_CRITICAL(flags);
-	size = (end - start + (PAGE_SIZE - 1)) >> PAGE_SHIFT;
-	size = (size + 1) >> 1;
-	if (size <= (current_cpu_data.tlbsizeftlbsets ?
-		     current_cpu_data.tlbsize / 8 :
-		     current_cpu_data.tlbsize / 2)) {
-		int pid = read_c0_entryhi();
-
-		start &= (PAGE_MASK << 1);
-		end += ((PAGE_SIZE << 1) - 1);
-		end &= (PAGE_MASK << 1);
-		htw_stop();
-
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-		emulate_tlb_ops(start, end, size, 0, 0x5004, 2);
-#else
-		while (start < end) {
-			int idx;
-
-			write_c0_entryhi(start);
-			start += (PAGE_SIZE << 1);
-			mtc0_tlbw_hazard();
-			tlb_probe();
-			tlb_probe_hazard();
-			idx = read_c0_index();
-			write_c0_entrylo0(0);
-			write_c0_entrylo1(0);
-			if (idx < 0)
-				continue;
-			/* Make sure all entries differ. */
-			write_c0_entryhi(UNIQUE_ENTRYHI(idx));
-			mtc0_tlbw_hazard();
-			tlb_write_indexed();
-		}
-#endif
-		tlbw_use_hazard();
-		write_c0_entryhi(pid);
-		htw_start();
-	} else {
-		local_flush_tlb_all();
-	}
-	flush_micro_tlb();
-	EXIT_CRITICAL(flags);
-}
-
-void local_flush_tlb_page(struct vm_area_struct *vma, unsigned long page)
-{
-	int cpu = smp_processor_id();
-
-	if (cpu_context(cpu, vma->vm_mm) != 0) {
-		unsigned long flags;
-#ifndef CONFIG_KVM_GUEST_LS3A3000
-		int oldpid, newpid, idx;
-#else
-		int oldpid, newpid;
-#endif
-
-		newpid = cpu_asid(cpu, vma->vm_mm);
-#ifndef CONFIG_KVM_GUEST_LS3A3000
-		page &= (PAGE_MASK << 1);
-#endif
-		ENTER_CRITICAL(flags);
-		oldpid = read_c0_entryhi();
-		htw_stop();
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-		write_c0_entryhi(newpid);
-
-		emulate_tlb_ops(page, page, 0, 0, 0x5001, 2);
-		goto finish;
-#else
-		write_c0_entryhi(page | newpid);
-		mtc0_tlbw_hazard();
-		tlb_probe();
-		tlb_probe_hazard();
-		idx = read_c0_index();
-		write_c0_entrylo0(0);
-		write_c0_entrylo1(0);
-		if (idx < 0)
-			goto finish;
-		/* Make sure all entries differ. */
-		write_c0_entryhi(UNIQUE_ENTRYHI(idx));
-		mtc0_tlbw_hazard();
-		tlb_write_indexed();
-		tlbw_use_hazard();
-
-#endif
-	finish:
-		write_c0_entryhi(oldpid);
-		htw_start();
-		flush_micro_tlb_vm(vma);
-		EXIT_CRITICAL(flags);
-	}
-}
-
-/*
- * This one is only used for pages with the global bit set so we don't care
- * much about the ASID.
- */
-void local_flush_tlb_one(unsigned long page)
-{
-	unsigned long flags;
-#ifndef CONFIG_KVM_GUEST_LS3A3000
-	int oldpid, idx;
-#else
-	int oldpid;
-#endif
-
-	ENTER_CRITICAL(flags);
-	oldpid = read_c0_entryhi();
-	htw_stop();
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-	emulate_tlb_ops(page, page, 0, 0, 0x5005, 2);
-#else
-	page &= (PAGE_MASK << 1);
-	write_c0_entryhi(page);
-	mtc0_tlbw_hazard();
-	tlb_probe();
-	tlb_probe_hazard();
-	idx = read_c0_index();
-	write_c0_entrylo0(0);
-	write_c0_entrylo1(0);
-	if (idx >= 0) {
-		/* Make sure all entries differ. */
-		write_c0_entryhi(UNIQUE_ENTRYHI(idx));
-		mtc0_tlbw_hazard();
-		tlb_write_indexed();
-		tlbw_use_hazard();
-	}
-#endif
-	write_c0_entryhi(oldpid);
-	htw_start();
-	flush_micro_tlb();
-	EXIT_CRITICAL(flags);
-}
-
-/*
- * We will need multiple versions of update_mmu_cache(), one that just
- * updates the TLB with the new pte(s), and another which also checks
- * for the R4k "end of page" hardware bug and does the needy.
- */
-void __update_tlb(struct vm_area_struct * vma, unsigned long address, pte_t pte)
-{
-	unsigned long flags;
-	pgd_t *pgdp;
-	pud_t *pudp;
-	pmd_t *pmdp;
-	pte_t *ptep;
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-	unsigned long pageshift,even_pte,odd_pte;
-	unsigned long tmp_address;
-	unsigned long vm_flags;
-#else
-	int idx, pid;
-#endif
-
-	/*
-	 * Handle debugger faulting in for debugee.
-	 */
-	if (current->active_mm != vma->vm_mm)
-		return;
-
-	ENTER_CRITICAL(flags);
-
-	htw_stop();
-
-#ifdef CONFIG_KVM_GUEST_LS3A3000
-	tmp_address = address;
-	address &= (PAGE_MASK << 1);
-	pgdp = pgd_offset(vma->vm_mm, address);
-	pudp = pud_offset(pgdp, address);
-	pmdp = pmd_offset(pudp, address);
-
-	ptep = pte_offset_map(pmdp, address);
-	pageshift= 14;
-	even_pte = pte_val(*ptep++);
-	odd_pte = pte_val(*ptep);
-
-	if((tmp_address >> PAGE_SHIFT) & 0x1)
-		if(odd_pte & _PAGE_DIRTY)
-			vm_flags = 3;
-		else
-			vm_flags = 2;
-	else
-		if(even_pte & _PAGE_DIRTY)
-			vm_flags = 3;
-		else
-			vm_flags = 2;
-
-	emulate_tlb_ops(tmp_address, pageshift, even_pte, odd_pte, 0x4000, vm_flags);
-#else
-	pid = read_c0_entryhi() & cpu_asid_mask(&current_cpu_data);
-	address &= (PAGE_MASK << 1);
-	write_c0_entryhi(address | pid);
-	pgdp = pgd_offset(vma->vm_mm, address);
-	mtc0_tlbw_hazard();
-	tlb_probe();
-	tlb_probe_hazard();
-	pudp = pud_offset(pgdp, address);
-	pmdp = pmd_offset(pudp, address);
-	idx = read_c0_index();
-#ifdef CONFIG_MIPS_HUGE_TLB_SUPPORT
-	/* this could be a huge page  */
-	if (pmd_huge(*pmdp)) {
-		unsigned long lo;
-		write_c0_pagemask(PM_HUGE_MASK);
-		ptep = (pte_t *)pmdp;
-		lo = pte_to_entrylo(pte_val(*ptep));
-		write_c0_entrylo0(lo);
-		write_c0_entrylo1(lo + (HPAGE_SIZE >> 7));
-
-		mtc0_tlbw_hazard();
-		if (idx < 0)
-			tlb_write_random();
-		else
-			tlb_write_indexed();
-		tlbw_use_hazard();
-		write_c0_pagemask(PM_DEFAULT_MASK);
-	} else
-#endif
-	{
-		ptep = pte_offset_map(pmdp, address);
-
-#if defined(CONFIG_64BIT_PHYS_ADDR) && defined(CONFIG_CPU_MIPS32)
-		write_c0_entrylo0(ptep->pte_high);
-		ptep++;
-		write_c0_entrylo1(ptep->pte_high);
-#else
-		write_c0_entrylo0(pte_to_entrylo(pte_val(*ptep++)));
-		write_c0_entrylo1(pte_to_entrylo(pte_val(*ptep)));
-#endif
-		mtc0_tlbw_hazard();
-		if (idx < 0)
-			tlb_write_random();
-		else
-			tlb_write_indexed();
-	}
-	tlbw_use_hazard();
-#endif
-	htw_start();
-	flush_micro_tlb_vm(vma);
-	EXIT_CRITICAL(flags);
 }
 
 void add_wired_entry(unsigned long entrylo0, unsigned long entrylo1,
